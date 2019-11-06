b80f177176e8389c34598a102f065d70e799421a
ethic41 <dahirmuhammad3@gmail.com>
2019-11-06 20:16:41 +0100

    completed chapter0

---
+++ accelerated_c++/chapter0/exercise2.cpp
@@ -0,0 +1,11 @@
+//chapter 0
+//exercise 0-2
+//write a program that when run writes
+//This (")is a quote, and this (\) is a backlash
+
+#include <iostream>
+
+int main(){
+    std::cout<<"This (\") is a quote, and this (\\) is a backlash"<<std::endl;
+    return 0;
+}
---
+++ accelerated_c++/chapter0/hello.cpp
@@ -0,0 +1,7 @@
+//a small c++ program
+#include <iostream>
+
+int main(){
+    std::cout<<"Hello, World"<<std::endl;
+    return 0;
+}
